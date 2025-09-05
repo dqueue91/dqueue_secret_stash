b20c5b6c27c194d95aa9de36e3f5c64efba902f4
dqueue91 <dqueue91@gmail.com>
2025-09-05 17:32:06 -0400

    initial commit

--- guiCTK.py
+++ guiCTK.py
@@ -2,35 +2,11 @@ import customtkinter as ctk
 from tkinter import messagebox
 import os
 from pathlib import Path
-import csv
+import bill_collector
 
 
 #main screen
 class main_menu(ctk.CTkFrame):
-    def __init__(self, master, switch_frame):
-        super().__init__(master)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        self.view_file = ctk.CTkButton(self, text="View Files", command=lambda: switch_frame(file_menu))
-        self.view_file.grid(row=1, column=0, padx=10, pady=10, sticky="w")
-        self.close = ctk.CTkButton(self, text="Exit", command=self.close_callback)
-        self.close.grid(row=2, column=0, padx=10, pady=10, sticky="w")
-
-    def view_file_callback(self):
-        self.switch_frame(file_menu)
-
-    def add_file_callback(self):
-        print("Adding file")
-  
-    def close_callback(self):
-        #first variable is window title, second variable is window text
-        answer = messagebox.askyesno("Confirm exit", "Are you sure you want to exit?")
-        if answer:
-            app.destroy()
-
-
-#after clicking view file button
-class file_menu(ctk.CTkFrame):
     # add file, edit file, delete
     def __init__(self, master, switch_frame):
         super().__init__(master)
@@ -38,8 +14,6 @@ class file_menu(ctk.CTkFrame):
         self.grid_columnconfigure(0, weight=1)
         self.add_file = ctk.CTkButton(self, text="Add File", command=self.add_file_callback)
         self.add_file.grid(row=1, column=0, padx=10, pady=10, sticky="w")
-        self.back = ctk.CTkButton(self, text="Previous Page", command=lambda: switch_frame(main_menu))
-        self.back.grid(row=2, column=0, padx=10, pady=10, sticky="w")
         self.close = ctk.CTkButton(self, text="Exit", command=self.close_callback)
         self.close.grid(row=3, column=0, padx=10, pady=10, sticky="w")
         #scrollable frame
@@ -54,9 +28,11 @@ class file_menu(ctk.CTkFrame):
             label.grid(row=i,column=0, padx=10, pady=10)
             label.bind("<Button-1>", lambda event, filename=f: self.on_file_click(filename))
 
+    def view_file_callback(self):
+        self.switch_frame(file_menu)
+
     def add_file_callback(self):
         print("Adding file")
-
   
     def close_callback(self):
         #first variable is window title, second variable is window text
@@ -65,17 +41,38 @@ class file_menu(ctk.CTkFrame):
             app.destroy()
 
     def on_file_click(self, filename):
-        with open(filename, 'r', newline='') as csvfile:
-            csv_reader = csv.DictReader(csvfile, fieldnames = fieldnames)
-                #print("Name     ","Price    ", "Date")
-                
-            for i in csv_reader:
-                name = i["Name"]
-                price = i["Price"]
-                date = i["Date"]
-                total = total + float(price)
-                print(f"{name} {price} due on the {date}")
-            print(f"Monthly expenses are: {total}")
+       bill_collector.read_file()
+
+
+#after clicking add file button
+class file_menu(ctk.CTkFrame):
+    # edit file, save, close
+    def __init__(self, master, switch_frame):
+        super().__init__(master)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.save = ctk.CTkButton(self, text="Save", command=self.save_file)
+        self.save.grid(row=1, column=2, padx=10, pady=10, sticky="w")
+        self.back = ctk.CTkButton(self, text="Previous Page", command=lambda: switch_frame(main_menu))
+        self.back.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+        self.close = ctk.CTkButton(self, text="Exit", command=self.close_callback)
+        self.close.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+        #scrollable frame
+        scroll_frame = ctk.CTkScrollableFrame(self, width=900, height=800)
+        scroll_frame.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+
+    def edit_file(self):
+        pass
+
+    def save_file(self):
+        pass
+  
+    def close_callback(self):
+        #first variable is window title, second variable is window text
+        answer = messagebox.askyesno("Confirm exit", "Are you sure you want to exit?")
+        if answer:
+            app.destroy()
+
 
 
 class App(ctk.CTk):
